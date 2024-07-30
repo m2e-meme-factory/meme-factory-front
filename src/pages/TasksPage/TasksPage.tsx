545b78cb73bee2936d27579e5a734e017b201ce5
ilyainavoid <thirdilno@gmail.com>
2024-07-30 17:42:58 +0700

    feat: add selects for filters

--- src/pages/TasksPage/TasksPage.tsx
+++ src/pages/TasksPage/TasksPage.tsx
@@ -1,22 +1,26 @@
-import {Text, Flex, Heading, Card} from "@radix-ui/themes";
+import {Text, Flex, Heading} from "@radix-ui/themes";
 import React from "react";
-import CardBanner from "./components/CardBanner/CardBanner";
-import styled from "styled-components";
-import TaskContent from "./components/TaskContent/TaskContent";
 import TaskCard from "./components/TaskCard/TaskCard";
+import Select from "../../components/Select/Select";
 
 const TasksPage = () => {
-    const StyledCard = styled(Card)`
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    background-color: #fecf0a;
-`;
+
+    const options = ['opt1', 'opt2', 'opt3'];
 
     return (<>
         <Flex m="4" direction="column">
             <Heading>Recent Tasks</Heading>
             <Text color="gray">Earn M2E by completing them</Text>
+        </Flex>
+        <Flex justify='between' p='4'>
+            <Flex direction='column'>
+                <Text mb='2' weight='medium'>Category:</Text>
+                <Select options={options}></Select>
+            </Flex>
+            <Flex direction='column'>
+                <Text mb='2' weight='medium'>Tags:</Text>
+                <Select options={options}></Select>
+            </Flex>
         </Flex>
         <Flex m="4" direction="column">
             <TaskCard/>

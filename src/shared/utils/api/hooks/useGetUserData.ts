ae50c99e80dde63127a82e2db333464903ce8cf0
ilyainavoid <thirdilno@gmail.com>
2024-07-31 13:16:35 +0700

    fix: handle undefined user id

--- src/shared/utils/api/hooks/useGetUserData.ts
+++ src/shared/utils/api/hooks/useGetUserData.ts
@@ -1,11 +1,14 @@
 import {useQuery} from "@tanstack/react-query";
 import {getUserData} from "../requests/getUserData";
 
-export const useGetUserData = (query_id: string, user_id: string) => {
+export const useGetUserData = (query_id: string, user_id?: string) => {
     const query = useQuery({
         queryKey: ['getUser', user_id],
         queryFn: () => {
-            return getUserData({params: {query_id, user_id}})
+            if (user_id) {
+                return getUserData({params: {query_id, user_id}});
+            }
+            return Promise.reject(new Error('ID пользователя не валиден!'));
         },
         select: (data) => data
     });

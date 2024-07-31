9dcf7d72c7aa29ce406018f1d6cb0e3ef7aab8cf
ilyainavoid <thirdilno@gmail.com>
2024-07-31 15:36:08 +0700

    fix: replace primitive selects with themed

--- src/pages/TasksPage/TasksPage.tsx
+++ src/pages/TasksPage/TasksPage.tsx
@@ -1,7 +1,8 @@
-import { Text, Flex, Heading } from '@radix-ui/themes';
+import { Text, Flex, Heading, Select } from '@radix-ui/themes';
 import React from 'react';
 import TaskCard from './components/TaskCard/TaskCard';
-import Select from '../../shared/components/Select/Select';
+import { CATEGORIES } from '../../shared/consts/categories';
+import { TAGS } from '../../shared/consts/tags';
 
 const TasksPage = () => {
   const options = ['opt1', 'opt2', 'opt3'];
@@ -12,18 +13,36 @@ const TasksPage = () => {
         <Heading>Recent Tasks</Heading>
         <Text color='gray'>Earn M2E by completing them</Text>
       </Flex>
-      <Flex justify='between' p='4'>
-        <Flex direction='column'>
+      <Flex justify='between' p='4' direction='column'>
+        <Flex direction='column' mb='5'>
           <Text mb='2' weight='medium'>
             Category:
           </Text>
-          <Select options={options}></Select>
+          <Select.Root>
+            <Select.Trigger />
+            <Select.Content>
+              <Select.Group>
+                {CATEGORIES.map((category, index) => (
+                  <Select.Item value={category.value}>{category.label}</Select.Item>
+                ))}
+              </Select.Group>
+            </Select.Content>
+          </Select.Root>
         </Flex>
         <Flex direction='column'>
           <Text mb='2' weight='medium'>
             Tags:
           </Text>
-          <Select options={options}></Select>
+          <Select.Root>
+            <Select.Trigger />
+            <Select.Content>
+              <Select.Group>
+                {TAGS.map((tag, index) => (
+                  <Select.Item value={tag.value}>{tag.label}</Select.Item>
+                ))}
+              </Select.Group>
+            </Select.Content>
+          </Select.Root>
         </Flex>
       </Flex>
       <Flex m='4' direction='column'>
---
+++ src/shared/consts/categories.ts
@@ -0,0 +1,34 @@
+export const CATEGORIES = [
+  {
+    value: 'discord',
+    label: 'Discord',
+  },
+  {
+    value: 'medium',
+    label: 'Medium',
+  },
+  {
+    value: 'instagram',
+    label: 'Instagram',
+  },
+  {
+    value: 'tiktok',
+    label: 'Tiktok',
+  },
+  {
+    value: 'youtube',
+    label: 'Youtube',
+  },
+  {
+    value: 'twitter',
+    label: 'Twitter',
+  },
+  {
+    value: 'telegram',
+    label: 'Telegram',
+  },
+  {
+    value: 'facebook',
+    label: 'Facebook',
+  },
+];

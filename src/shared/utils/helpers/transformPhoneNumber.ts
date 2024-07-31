1381bc9b693b201b98ef4b4013dce9f396e92d58
ilyainavoid <thirdilno@gmail.com>
2024-07-31 13:16:03 +0700

    refactor: make in;ine returns in transformPhoneNumber

---
+++ src/shared/utils/helpers/transformPhoneNumber.ts
@@ -0,0 +1,11 @@
+export const transformPhoneNumber = (phoneNumber: string) => {
+    let phoneRegex =
+        /^\+?(\d{1,3})?[\s\-]?\(?(\d{3})\)?[\s\-]?(\d{3})[\s\-]?(\d{2})[\s\-]?(\d{2})$/;
+    let match = phoneNumber.match(phoneRegex);
+
+    if (match) {
+        return `${match[1]}${match[2]}${match[3]}${match[4]}${match[5]}`;
+    } else {
+        return null;
+    }
+};

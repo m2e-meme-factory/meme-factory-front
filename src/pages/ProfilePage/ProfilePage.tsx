afed80fe3031dfee521250aea5874ccb02f29ab0
ilyainavoid <thirdilno@gmail.com>
2024-07-31 17:12:05 +0700

    feat: add balance and nav tabs

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { Grid, Text, Card, DataList, Badge, Heading, Spinner, Flex } from '@radix-ui/themes';
+import {
+  Grid,
+  Text,
+  Card,
+  DataList,
+  Badge,
+  Heading,
+  Spinner,
+  Flex,
+  Button,
+  IconButton,
+  Box,
+  Tabs,
+} from '@radix-ui/themes';
 import { useTelegram } from '../../shared/hooks/useTelegram';
 import CopyableCode from '../../shared/components/CopyableCode';
 import CopyableTextField from '../../shared/components/CopyableTextField';
 import { useGetRefData } from '../../shared/utils/api/hooks/useGetRefData';
 import { useGetUserData } from '../../shared/utils/api/hooks/useGetUserData';
+import { ChevronRightIcon } from '@radix-ui/react-icons';
 import styles from './ProfilePage.module.css';
 
 export default function ProfilePage() {
@@ -30,66 +44,104 @@ export default function ProfilePage() {
   const refUUID = userData?.ref_uuid || '';
 
   return (
-    <>
-      <Card m='4'>
-        <Grid gap='4'>
-          <Heading>Profile</Heading>
-          <DataList.Root>
-            <DataList.Item align='center'>
-              <DataList.Label minWidth='88px'>Status</DataList.Label>
-              <DataList.Value>
-                <Badge color='jade' variant='soft' radius='full'>
-                  Authorized
-                </Badge>
-              </DataList.Value>
-            </DataList.Item>
-            <DataList.Item>
-              <DataList.Label minWidth='88px'>ID</DataList.Label>
-              <DataList.Value>
-                <CopyableCode value={userId || ''} />
-              </DataList.Value>
-            </DataList.Item>
-            <DataList.Item>
-              <DataList.Label minWidth='88px'>Name</DataList.Label>
-              <DataList.Value>{`${user?.last_name} ${user?.first_name}`}</DataList.Value>
-            </DataList.Item>
-            <DataList.Item>
-              <DataList.Label minWidth='88px'>Nickname</DataList.Label>
-              <DataList.Value>
-                <CopyableCode value={`${user?.username}`} />
-              </DataList.Value>
-            </DataList.Item>
-            <DataList.Item>
-              <DataList.Label minWidth='88px'>Type</DataList.Label>
-              <DataList.Value>Creator</DataList.Value>
-            </DataList.Item>
-          </DataList.Root>
-        </Grid>
-      </Card>
+    <Tabs.Root defaultValue='account'>
+      <Tabs.List justify='center' highContrast>
+        <Tabs.Trigger value='account'>Account</Tabs.Trigger>
+        <Tabs.Trigger value='wallet'>Wallet</Tabs.Trigger>
+        <Tabs.Trigger value='socials'>Socials</Tabs.Trigger>
+      </Tabs.List>
 
-      <Card m='4'>
-        <Grid gap='4'>
-          <Heading>Referas</Heading>
-          <Text color='gray'>Your Ref link:</Text>
-          <CopyableTextField
-            size={'2'}
-            fieldSize='3'
-            value={`https://t.me/m2e_meme_factory_bot?start=ref_${refUUID}`}
-          />
-          <DataList.Root mt='4'>
-            <DataList.Item align='center'>
-              <DataList.Item>
-                <DataList.Label minWidth='88px'>Total Count</DataList.Label>
-                <DataList.Value>{refCount}</DataList.Value>
-              </DataList.Item>
-              <DataList.Item>
-                <DataList.Label minWidth='88px'>Total profit</DataList.Label>
-                <DataList.Value>0 M2E</DataList.Value>
-              </DataList.Item>
-            </DataList.Item>
-          </DataList.Root>
-        </Grid>
-      </Card>
-    </>
+      <Box pt='3'>
+        <Tabs.Content value='account'>
+          <Card m='4'>
+            <Grid gap='4'>
+              <Heading>Profile</Heading>
+              <DataList.Root>
+                <DataList.Item align='center'>
+                  <DataList.Label minWidth='88px'>Status</DataList.Label>
+                  <DataList.Value>
+                    <Badge color='jade' variant='soft' radius='full'>
+                      Authorized
+                    </Badge>
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label minWidth='88px'>ID</DataList.Label>
+                  <DataList.Value>
+                    <CopyableCode value={userId || ''} />
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label minWidth='88px'>Name</DataList.Label>
+                  <DataList.Value>{`${user?.last_name} ${user?.first_name}`}</DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label minWidth='88px'>Nickname</DataList.Label>
+                  <DataList.Value>
+                    <CopyableCode value={`${user?.username}`} />
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label minWidth='88px'>Type</DataList.Label>
+                  <DataList.Value>Creator</DataList.Value>
+                </DataList.Item>
+              </DataList.Root>
+            </Grid>
+          </Card>
+
+          <Card m='4'>
+            <Flex justify='between' align='center'>
+              <Flex direction='column'>
+                <Text mb='2' color='gray'>
+                  Available Balance
+                </Text>
+                <Heading>$26 412.03</Heading>
+              </Flex>
+              <Button>
+                <ChevronRightIcon /> Withdraw
+              </Button>
+            </Flex>
+          </Card>
+
+          <Card m='4'>
+            <Grid gap='4'>
+              <Heading>Referas</Heading>
+              <Text color='gray'>Your Ref link:</Text>
+              <CopyableTextField
+                size={'2'}
+                fieldSize='3'
+                value={`https://t.me/m2e_meme_factory_bot?start=ref_${refUUID}`}
+              />
+              <DataList.Root mt='4'>
+                <DataList.Item align='center'>
+                  <DataList.Item>
+                    <DataList.Label minWidth='88px'>Total Count</DataList.Label>
+                    <DataList.Value>{refCount}</DataList.Value>
+                  </DataList.Item>
+                  <DataList.Item>
+                    <DataList.Label minWidth='88px'>Total profit</DataList.Label>
+                    <DataList.Value>0 M2E</DataList.Value>
+                  </DataList.Item>
+                </DataList.Item>
+              </DataList.Root>
+            </Grid>
+          </Card>
+        </Tabs.Content>
+
+        <Tabs.Content value='wallet'>
+          <Card m='4'>
+            <Heading>Wallet</Heading>
+            <button className={styles.ConnectButton}>Connect Wallet</button>
+          </Card>
+        </Tabs.Content>
+
+        <Tabs.Content value='socials'>
+          <Card m='4'>
+            <Heading>Socials</Heading>
+            <button className={styles.ConnectButton}>Connect Socials</button>
+          </Card>
+        </Tabs.Content>
+      </Box>
+    </Tabs.Root>
   );
 }

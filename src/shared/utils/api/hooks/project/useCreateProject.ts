cf81b0a7e0fc3a1bbae584dccc6d66f148997661
ilyainavoid <thirdilno@gmail.com>
2024-08-05 13:37:30 +0700

    feat: project request hooks

---
+++ src/shared/utils/api/hooks/project/useCreateProject.ts
@@ -0,0 +1,8 @@
+import { useMutation } from '@tanstack/react-query';
+import { createProject, CreateProjectConfig } from '../../requests/project/project-requests';
+
+export const useCreateProject = () => {
+  useMutation({
+    mutationFn: (config: CreateProjectConfig) => createProject(config),
+  });
+};
---
+++ src/shared/utils/api/hooks/project/useDeleteProject.ts
@@ -0,0 +1,8 @@
+import { useMutation } from '@tanstack/react-query';
+import { deleteProject, GetOrDeleteProjectConfig } from '../../requests/project/project-requests';
+
+export const useDeleteProject = () => {
+  useMutation({
+    mutationFn: (config: GetOrDeleteProjectConfig) => deleteProject(config),
+  });
+};

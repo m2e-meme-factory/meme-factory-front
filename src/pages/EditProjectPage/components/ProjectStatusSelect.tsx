52006ada8fece0f261769997421d49912435f559
ilyainavoid <thirdilno@gmail.com>
2024-08-09 20:01:30 +0700

    fix: edit status change

--- src/pages/EditProjectPage/components/ProjectStatusSelect.tsx
+++ src/pages/EditProjectPage/components/ProjectStatusSelect.tsx
@@ -1,11 +1,5 @@
-import { Button, Flex, AlertDialog } from '@radix-ui/themes';
-import Select, { SingleValue } from 'react-select';
-import { PROJECT_STATUSES } from '../../../shared/consts/project-statuses';
-import { CUSTOM_SELECT_STYLES_SINGLE } from '../../../styles/customSelectStyles';
-import React, { FC, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDeleteProject } from '../../../shared/utils/api/hooks/project/useDeleteProject';
-import { Option } from '../../../@types/app';
+import { Button, Flex, AlertDialog, Heading } from '@radix-ui/themes';
+import React, { FC } from 'react';
 import { useUpdateProjectStatus } from '../../../shared/utils/api/hooks/project/useUpdateProjectStatus';
 
 enum ProjectStatus {
@@ -22,37 +16,47 @@ interface ProjectStatusSelect {
 }
 
 const ProjectStatusSelect: FC<ProjectStatusSelect> = ({ projectId, projectStatus }) => {
-  const navigate = useNavigate();
-  const [currentStatus, setCurrentStatus] = useState<ProjectStatus>(projectStatus);
-  const deleteProjectMutation = useDeleteProject();
   const updateProjectStatus = useUpdateProjectStatus(projectId);
 
-  const handleStatusChange = (status: SingleValue<Option>) => {
-    if (status) {
-      const statusValue = status.value as ProjectStatus;
-      setCurrentStatus(statusValue);
-    }
-  };
+  const handleSendOnModeration = () => {
+    updateProjectStatus.mutate({ params: { id: projectId, payload: { status: ProjectStatus.MODERATION } } });
+  }
 
-  const handleChangeStatusClick = () => {
-    updateProjectStatus.mutate({ params: { id: projectId, payload: { status: currentStatus } } });
-  };
+  const handleProjectDelete = () => {
+    updateProjectStatus.mutate({ params: { id: projectId, payload: { status: ProjectStatus.CLOSED } } });
+  }
 
   return (
     <Flex direction='column'>
-      <Select
-        onChange={handleStatusChange}
-        placeholder='Select status'
-        closeMenuOnSelect={true}
-        options={PROJECT_STATUSES}
-        styles={CUSTOM_SELECT_STYLES_SINGLE}
-        value={currentStatus}
-        isMulti={false}
-      />
+      <Heading>Change project status:</Heading>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button style={{ padding: '20px', marginTop: '10px' }}>Send on moderation</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth='450px'>
+          <AlertDialog.Title>Send on moderation</AlertDialog.Title>
+          <AlertDialog.Description size='2'>
+            Are you sure? The project will be unavailable for editing once sent on moderation.
+          </AlertDialog.Description>
+
+          <Flex gap='3' mt='4' justify='end'>
+            <AlertDialog.Cancel>
+              <Button variant='soft' color='gray'>
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button onClick={handleSendOnModeration} variant='solid' color='yellow'>
+                Submit
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
 
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button style={{ marginTop: '20px', marginBottom: '20px' }}>Change status</Button>
+          <Button color='red' style={{ padding: '20px', marginTop: '10px' }}>Delete project</Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content maxWidth='450px'>
           <AlertDialog.Title>Delete project</AlertDialog.Title>
@@ -68,7 +72,7 @@ const ProjectStatusSelect: FC<ProjectStatusSelect> = ({ projectId, projectStatus
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button onClick={handleChangeStatusClick} variant='solid' color='red'>
+              <Button onClick={handleProjectDelete} variant='solid' color='red'>
                 Change status
               </Button>
             </AlertDialog.Action>

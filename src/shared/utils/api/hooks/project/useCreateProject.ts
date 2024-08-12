28b6d1e6321fd1bb093c80c20a0d9a071caedfd4
ilyainavoid <thirdilno@gmail.com>
2024-08-12 17:52:00 +0700

    feat: add notifications and files upload in edit project page

--- src/shared/utils/api/hooks/project/useCreateProject.ts
+++ src/shared/utils/api/hooks/project/useCreateProject.ts
@@ -4,7 +4,12 @@ import { Dispatch, SetStateAction, useState } from 'react';
 import { useTelegram } from '../../../../hooks/useTelegram';
 import { createProject, CreateProjectConfig } from '../../requests/project/project-requests';
 import { login, LoginConfig } from '../../requests/auth/login';
-
+import {
+  showErrorMessage,
+  showSuccessMessage,
+  showToastWithPromise,
+} from '../../../helpers/notify';
+import toast from 'react-hot-toast';
 
 export const useCreateProject = (setCreateLoading: Dispatch<SetStateAction<boolean>>) => {
   const navigate = useNavigate();
@@ -15,6 +20,7 @@ export const useCreateProject = (setCreateLoading: Dispatch<SetStateAction<boole
     mutationFn: (config: CreateProjectConfig) => createProject(config),
     onSuccess: () => {
       setCreateLoading(false);
+      showSuccessMessage('Project created successfully');
       navigate('/projects');
     },
     onMutate: (variables) => {
@@ -25,21 +31,42 @@ export const useCreateProject = (setCreateLoading: Dispatch<SetStateAction<boole
 
       if (error?.response?.status === 401 && webApp) {
         const loginConfig: LoginConfig = {
-          params: { initData: { initData: webApp.initData } }
+          params: { initData: { initData: webApp.initData } },
         };
 
         try {
-          const response = await login(loginConfig);
+          const response = await toast.promise(
+            login(loginConfig),
+            {
+              success: 'Logged in successfully',
+              error: 'Failed to sign in',
+              loading: 'Logging in',
+            },
+            {
+              style: {
+                borderRadius: '10px',
+                background: '#333',
+                color: '#fff',
+              },
+            }
+          );
           const newToken = response.data.token;
           localStorage.setItem('token', newToken);
 
           if (savedVariables) {
-            await createProject(savedVariables);
+            await showToastWithPromise({
+              success: 'Project created successfully',
+              error: 'Error while creating project',
+              process: 'Creating a project',
+              callback: () => createProject(savedVariables),
+            });
             navigate('/projects');
           }
         } catch (loginError) {
-          console.error('Login failed:', loginError);
+          showErrorMessage('Failed to create project due to authorization issue!');
         }
+      } else {
+        showErrorMessage('Something went wrong!');
       }
     },
   });

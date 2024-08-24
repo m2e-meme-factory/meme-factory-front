eb3e3f3b562b7c429e982eb6aa6d2ff1ef79eca9
ilyainavoid <thirdilno@gmail.com>
2024-08-24 16:18:31 +0700

    feat: auto tasks

--- src/pages/PublicProjectsPage/PublicProjectsPage.tsx
+++ src/pages/PublicProjectsPage/PublicProjectsPage.tsx
@@ -12,9 +12,15 @@ import {
   CUSTOM_SELECT_STYLES_MULTI,
   CUSTOM_SELECT_STYLES_SINGLE,
 } from '../../styles/customSelectStyles';
-import { Project } from 'api';
+import { Project, ProjectProgress, User } from 'api';
 import { useInView } from 'react-intersection-observer';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../shared/utils/redux/store';
+import { useGetRefData } from '../../shared/utils/api/hooks/user/useGetRefData';
+import SubtaskCard from '../ProjectPage/components/SubtaskCard/SubtaskCard';
+import { UserRoleInProject } from '../ProjectPage/ProjectPage';
+import AutotaskCard from './components/AutoTask/AutoTask';
 
 const BlockObserver = styled.div`
   height: 40px;
@@ -23,6 +29,8 @@ const BlockObserver = styled.div`
 
 const PublicProjectsPage = () => {
   const loadedPages = useRef(new Set<number>());
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user.user);
 
   const [tempTags, setTempTags] = useState<string[]>([]);
   const [tempCategory, setTempCategory] = useState<string | null>(null);
@@ -32,6 +40,8 @@ const PublicProjectsPage = () => {
   const [projects, setProjects] = useState<Project[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [isEnd, setIsEnd] = useState(false);
+  const [refsCount, setRefsCount] = useState<number>(0);
+  const [autoTaskDone, setAutoTaskDone] = useState<boolean>(false);
 
   const DISPLAY_LIMIT = 10;
 
@@ -47,9 +57,17 @@ const PublicProjectsPage = () => {
     limit: DISPLAY_LIMIT,
   });
 
+  const { data: refResponse, isLoading: refLoading } = useGetRefData(user?.telegramId);
+
   const { ref, inView } = useInView({
     threshold: 1.0,
   });
+
+  useEffect(() => {
+    if (user && refResponse) {
+      setRefsCount(refResponse.data.count);
+    }
+  }, [refResponse]);
 
   useEffect(() => {
     if (data && !loadedPages.current.has(currentPage)) {
@@ -138,6 +156,16 @@ const PublicProjectsPage = () => {
         <Button mt='3' onClick={handleFindButtonClick}>
           Find
         </Button>
+      </Flex>
+      <Flex m='4'>
+        {refsCount === 0 && !autoTaskDone && (
+          <AutotaskCard
+            setAutoTaskDone={setAutoTaskDone}
+            title='Points for friends!'
+            description={`Earn rewards by inviting your friends to join our app! Simply share your unique referral link with your friends, and when they sign up using your link, both of you will receive bonus points. It's easy and a great way to enjoy the app together while earning extra rewards. Start sharing and watch your points grow as your friends join the fun!`}
+            price={1337}
+          />
+        )}
       </Flex>
       <Flex m='4' direction='column'>
         {projects.map((project, index) => (

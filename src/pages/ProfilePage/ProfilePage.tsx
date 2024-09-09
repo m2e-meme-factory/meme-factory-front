4d639cb5444b2056d50e74851b073d5e6937b024
ilyainavoid <thirdilno@gmail.com>
2024-09-09 16:12:15 +0700

    fix: move profile settings to profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -11,21 +11,26 @@ import {
   IconButton,
   Box,
   Tabs,
+  Dialog,
 } from '@radix-ui/themes';
 import CopyableCode from '../../shared/components/CopyableCode';
 import CopyableTextField from '../../shared/components/CopyableTextField';
 import { useGetRefData } from '../../shared/utils/api/hooks/user/useGetRefData';
-import { ChevronRightIcon, GearIcon } from '@radix-ui/react-icons';
+import { ArrowLeftIcon, ChevronRightIcon, GearIcon } from '@radix-ui/react-icons';
 import styles from './ProfilePage.module.css';
 import { Link } from 'react-router-dom';
 import MyProjectsPage from '../MyProjectsPage/MyProjectsPage';
 import TransactionsHistoryPage from '../TransactionsHistoryPage/TransactionsHistoryPage';
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 import { RefDataResponse, User } from 'api';
 import { useAuthMe } from '../../shared/utils/api/hooks/auth/useAuthMe';
 import { setUser } from '../../shared/utils/redux/user/userSlice';
 import { useSearchParams } from 'react-router-dom';
+import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
+import { RootState } from '../../shared/utils/redux/store';
+import { useVerifyUser } from '../../shared/utils/api/hooks/user/useVerifyUser';
+import { connectWallet } from '../../shared/utils/api/requests/ton/connect';
 
 export default function ProfilePage() {
   const dispatch = useDispatch();
@@ -34,6 +39,41 @@ export default function ProfilePage() {
   const [refData, setRefData] = useState<RefDataResponse | null>(null);
   const [searchParams] = useSearchParams();
   const defaultTab = searchParams.get('tab') || 'account';
+
+  const [walletAddress, setWalletAddress] = useState<string>();
+  const user = useSelector((state: RootState) => state.user.user);
+  const [tonConnectUI] = useTonConnectUI();
+
+  const verifyMutation = useVerifyUser();
+
+  useEffect(() => {
+    const unsubscribe = tonConnectUI.onStatusChange((wallet) => {
+      if (wallet) {
+        setWalletAddress(wallet.account.address);
+      } else {
+        setWalletAddress('');
+      }
+    });
+
+    return () => unsubscribe();
+  }, [tonConnectUI]);
+
+  useEffect(() => {
+    const connect = async (wallet: string) => {
+      await connectWallet({ params: { tonWalletAddress: wallet } });
+    };
+
+    if (walletAddress) {
+      console.log(walletAddress);
+      connect(walletAddress);
+    }
+  }, [walletAddress]);
+
+  const handleVerify = () => {
+    if (user) {
+      verifyMutation.mutate({ params: { userId: user.id } });
+    }
+  };
 
   useEffect(() => {
     if (userDataResponse) {
@@ -141,6 +181,52 @@ export default function ProfilePage() {
                 </DataList.Item>
               </DataList.Root>
             </Grid>
+          </Card>
+
+          <Card m='4'>
+            <Heading>Socials</Heading>
+            <button className={styles.ConnectButton}>Connect Socials</button>
+          </Card>
+
+          <Card m='4'>
+            <Heading>Verify</Heading>
+            <Text color='gray' size='2'>
+              Verified users have auto approve for any project apply and have 100% chance for
+              receiving airdrop. Instant verification price: 5 USDT
+            </Text>
+
+            <Dialog.Root>
+              <Dialog.Trigger>
+                <button className={styles.ConnectButton}>Verify now</button>
+              </Dialog.Trigger>
+
+              <Dialog.Content maxWidth='450px'>
+                <Dialog.Title>Verify now</Dialog.Title>
+                <Dialog.Description size='2' mb='4'>
+                  Are you sure you want to verify your profile?
+                </Dialog.Description>
+
+                <Flex gap='3' mt='4' justify='end'>
+                  <Dialog.Close>
+                    <Button variant='soft' color='gray'>
+                      Cancel
+                    </Button>
+                  </Dialog.Close>
+                  <Dialog.Close>
+                    <Button onClick={handleVerify}>Verify</Button>
+                  </Dialog.Close>
+                </Flex>
+              </Dialog.Content>
+            </Dialog.Root>
+          </Card>
+
+          <Card m='4'>
+            <Flex align='center' justify='between'>
+              <Heading size='4' mr='6'>
+                Connect TON Wallet
+              </Heading>
+              <TonConnectButton />
+            </Flex>
           </Card>
         </Tabs.Content>
 

f31ce5d75e13a7bf2c95cff5921b04ec88328b8c
egor-jan <eeleskin.egr@gmail.com>
2024-09-17 15:38:31 +0700

    fix: tasks icons

--- src/shared/utils/helpers/getIconByTaskId.tsx
+++ src/shared/utils/helpers/getIconByTaskId.tsx
@@ -1,20 +1,61 @@
 import React from 'react';
 import {
+  DiscordOutlined,
   FacebookOutlined,
   InstagramOutlined,
   LaptopOutlined,
+  MediumOutlined,
   RocketOutlined,
   SendOutlined,
+  TikTokOutlined,
   UserAddOutlined,
   XOutlined,
   YoutubeOutlined,
 } from '@ant-design/icons';
 import { ReactNode } from 'react';
+import RadditOutlined from '../../imgs/RadditOutlined';
 
 type IconType = {
   [key: number]: ReactNode;
 };
 
+/*
+
+export const tasks: Autotask[] = [
+  {
+    id: 7,
+    title: 'Follow us on Instagram',
+    description: `Follow MemeFactory on Instagram to stay updated with our latest posts, stories, and exclusive content. `,
+    reward: 100,
+    category: 'Instagram',
+    url: 'https://www.instagram.com/m2e__pro/',
+  },
+  {
+    id: 8,
+    title: 'Visit our Raddit',
+    description: `Show your support by liking our post on Raddit.`,
+    reward: 100,
+    category: 'Raddit',
+    url: 'https://www.reddit.com/user/m2epro/',
+  },
+  {
+    id: 9,
+    title: 'Like our post on Medium',
+    description: `Show your support by liking our post on Medium.`,
+    reward: 100,
+    category: 'Medium',
+    url: 'https://medium.com/@mail_13487',
+  },
+  {
+    id: 10,
+    title: 'Join Discord',
+    description: `Become a part of the Meme Factory community by joining our Discord channel.`,
+    reward: 100,
+    category: 'Discord',
+    url: 'https://discord.com/channels/@me',
+  },
+];
+*/
 const icons: IconType = {
   1: (
     <UserAddOutlined
@@ -36,12 +77,27 @@ const icons: IconType = {
     />
   ),
   6: (
-    <InstagramOutlined
+    <TikTokOutlined
       style={{ color: 'var(--gray-10)', margin: '0 15px 0 10px', fontSize: '24px' }}
     />
   ),
   7: (
-    <FacebookOutlined
+    <InstagramOutlined
+      style={{ color: 'var(--gray-10)', margin: '0 15px 0 10px', fontSize: '24px' }}
+    />
+  ),
+  8: (
+    <RadditOutlined 
+      style={{ fill: 'var(--gray-10)', margin: '0 15px 0 10px', height: '24px' }} 
+    />
+  ),
+  9: (
+    <MediumOutlined 
+      style={{ color: 'var(--gray-10)', margin: '0 15px 0 10px', fontSize: '24px' }}
+    />
+  ),
+  10: (
+    <DiscordOutlined 
       style={{ color: 'var(--gray-10)', margin: '0 15px 0 10px', fontSize: '24px' }}
     />
   ),

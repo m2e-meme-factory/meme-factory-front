2c6390a5c5fe4bd9107700de279fc769c07249f3
ilyainavoid <thirdilno@gmail.com>
2024-10-15 21:04:15 +0700

    feat: expand after init

--- src/ProviderWrapper.tsx
+++ src/ProviderWrapper.tsx
@@ -1,31 +1,20 @@
-import { TelegramProvider } from './shared/hooks/useTelegram';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './shared/utils/redux/store';
-import { THEME, TonConnectUI, TonConnectUIProvider } from '@tonconnect/ui-react';
-import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { useExpand, WebAppProvider } from '@vkruglikov/react-telegram-web-app';
 
 const queryClient = new QueryClient();
 const manifestUrl =
   'https://api.meme-factory.site/uploads/files/3477b352-6cf7-4967-9b1f-fe96fc925753_tonconnect-manifest.json';
 
-// const tonConnectUI = new TonConnectUI({
-//   manifestUrl,
-//   uiPreferences: {
-//     theme: THEME.DARK,
-//     borderRadius: "s",
-//     colorsSet: {
-//       DARK: {
-//         connectButton: {
-//           background: '#29CC6A'
-//         }
-//       },
-//     }
-//   }
-// });
-
 const ProviderWrapper = ({ children }: { children: ReactNode }) => {
+  const [isExpanded, expand] = useExpand();
+  useEffect(() => {
+    expand();
+  }, []);
+
   return (
     <TonConnectUIProvider manifestUrl={manifestUrl}>
       <WebAppProvider

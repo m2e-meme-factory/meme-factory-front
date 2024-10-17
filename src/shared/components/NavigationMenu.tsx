a995aa09738165eb492201690e8c991fead4fe4f
ilyainavoid <thirdilno@gmail.com>
2024-10-17 18:40:40 +0700

    feat: add tasks route and edit nav height

--- src/shared/components/NavigationMenu.tsx
+++ src/shared/components/NavigationMenu.tsx
@@ -2,23 +2,22 @@ import { Box, Button, Card, Flex, Separator, Text } from '@radix-ui/themes';
 import { Link, useLocation } from 'react-router-dom';
 import { navigationRoutes } from '../consts/navigationRoutes';
 import React from 'react';
-import { HomeIcon } from '@radix-ui/react-icons';
 
 const NavigationMenu = () => {
   const location = useLocation();
 
   return (
-    <Box style={{ position: 'fixed', bottom: '0', width: '100%' }}>
+    <Box style={{ position: 'fixed', bottom: '0', width: '100%', height: '10vh' }}>
       <Flex asChild width='100%' justify='center'>
-        <Card style={{ background: 'var(--gray-1)' }}>
-          <Flex gap='5' align='center' justify='center'>
+        <Card style={{ background: 'var(--gray-1)', height: '100%' }}>
+          <Flex gap='4' align='center' justify='center'>
             {navigationRoutes.map((route, key) => (
               <React.Fragment key={key}>
                 <Link to={route.path}>
                   <Button
                     size='3'
                     variant={'ghost'}
-                    color={location.pathname.includes(route.path) ? 'amber' : 'gray'}
+                    color={location.pathname === route.path ? 'amber' : 'gray'} // Изменено здесь
                   >
                     <Flex direction='column' align='center' justify='center'>
                       {route.icon}
@@ -26,7 +25,7 @@ const NavigationMenu = () => {
                     </Flex>
                   </Button>
                 </Link>
-                {key != navigationRoutes.length - 1 && (
+                {key !== navigationRoutes.length - 1 && (
                   <Separator size='2' orientation='vertical' />
                 )}
               </React.Fragment>

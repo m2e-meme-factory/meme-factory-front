9f6dd25f75d09c6d9bd62425e16e3c2042e37aa7
ilyainavoid <thirdilno@gmail.com>
2024-10-17 20:27:34 +0700

    feat: share button and copy on card click

--- src/pages/FriendsPage/Friends.tsx
+++ src/pages/FriendsPage/Friends.tsx
@@ -1,6 +1,17 @@
-import { Badge, Box, Card, DataList, Flex, Grid, Heading, Skeleton, Text } from '@radix-ui/themes';
+import {
+  Badge,
+  Box,
+  Button,
+  Card,
+  DataList,
+  Flex,
+  Grid,
+  Heading,
+  Skeleton,
+  Text,
+} from '@radix-ui/themes';
 import { useGetRefData } from '../../shared/utils/api/hooks/user/useGetRefData';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../shared/utils/redux/store';
 import { RefDataResponse } from 'api';
@@ -11,6 +22,7 @@ import GlowingButton from '../../shared/components/Buttons/GlowingButton';
 import WebappBackButton from '../../shared/components/WebappBackButton';
 import { useWebApp } from '@vkruglikov/react-telegram-web-app';
 import CopyableRef from '../AutotasksProject/components/CopyableField/CopyableRef';
+import { showSuccessMessage } from '../../shared/utils/helpers/notify';
 
 const ResponsibleImage = styled.img`
   height: 100px;
@@ -39,15 +51,74 @@ export default function Friends() {
   const user = useSelector((state: RootState) => state.user.user);
   const { data, isLoading: refLoading } = useGetRefData(user?.telegramId);
   const [refData, setRefData] = useState<RefDataResponse | null>(null);
-  const [isCalloutVisible, setCalloutVisible] = useState<boolean>(true);
+  const webApp = useWebApp();
+  const [copied, setCopied] = useState(false);
+  const [copyText, setCopyText] = useState<'copied' | 'click on me'>('click on me');
+  const copyableFieldRef = useRef<HTMLDivElement | null>(null);
+
+  const handleCopyText = (text: string) => {
+    const textToCopy = text;
+
+    if (navigator.clipboard && !copied) {
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => {
+          showSuccessMessage('Copied');
+          setCopyText('copied');
+          setCopied(true);
+
+          setTimeout(() => {
+            setCopied(false);
+            setCopyText('click on me');
+          }, 5000);
+        })
+        .catch(() => {
+          console.error('Clipboard copy failed, using fallback method.');
+          copyFallback(textToCopy);
+        });
+    } else if (!copied) {
+      copyFallback(textToCopy);
+    }
+  };
+
+  const copyFallback = (text: string) => {
+    const tempInput = document.createElement('input');
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    tempInput.setSelectionRange(0, text.length);
+
+    try {
+      document.execCommand('copy');
+      showSuccessMessage('Copied!');
+      setCopyText('copied');
+      setCopied(true);
+
+      setTimeout(() => {
+        setCopied(false);
+        setCopyText('click on me');
+      }, 5000);
+    } catch (error) {
+      console.error('Fallback copy failed.', error);
+    }
+
+    document.body.removeChild(tempInput);
+  };
+
+  const handleInviteClick = () => {
+    handleCopyText(refData?.refLink || '');
+  };
+
+  const handleShareClick = () => {
+    const shareUrl = `https://t.me/share/url?url=${encodeURIComponent(refData?.refLink || '')}&text=${encodeURIComponent('Earn with me!')}`;
+    webApp.openTelegramLink(shareUrl);
+  };
 
   useEffect(() => {
     if (data) {
       setRefData(data);
     }
   }, [data]);
-
-  const webApp = useWebApp();
 
   return (
     <>
@@ -61,7 +132,7 @@ export default function Friends() {
 
       <Box m='4'>
         <Flex direction='column' gap='2'>
-          <Card>
+          <Card onClick={handleInviteClick}>
             <Flex gap='4' align='center' p='1'>
               <Box>
                 <Text size='8' weight='bold'>
@@ -69,7 +140,7 @@ export default function Friends() {
                 </Text>
               </Box>
               <Box>
-                <Box>Invite friend</Box>
+                <Box>Invite friend ({copyText})</Box>
                 <Box>
                   <Text size='1' color='gray'>
                     Copy Link and send it to your friend
@@ -130,7 +201,7 @@ export default function Friends() {
 
             <Box asChild width='100%'>
               <Skeleton loading={refLoading}>
-                <CopyableRef refLink={refData?.refLink ?? ''} />
+                <Button onClick={handleShareClick}>Share</Button>
               </Skeleton>
             </Box>
             <DataList.Root mt='4'>

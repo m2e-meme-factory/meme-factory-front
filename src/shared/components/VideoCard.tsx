9c40e942c0190d1bf4f4208a964eb3469c0dc72e
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2025-02-05 00:07:27 +0300

    fix: video

--- src/shared/components/VideoCard.tsx
+++ src/shared/components/VideoCard.tsx
@@ -1,98 +1,101 @@
-import { Card } from "@radix-ui/themes";
-import React, { useState } from "react";
-import styled from "styled-components";
+import { Card } from '@radix-ui/themes';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 interface VideoCardProps {
-    videoSrc: string; // URL видео
-    thumbnailSrc: string; // URL превью
-    altText?: string; // Альтернативный текст для превью
+  videoSrc: string; // URL видео
+  thumbnailSrc: string; // URL превью
+  altText?: string; // Альтернативный текст для превью
 }
 
 const BoucingButton = styled.button`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 12px 15px;
-    background: rgba(0, 0, 0, 0.7);
-    color: #fecf0a;
-    border: none;
-    border-radius: 50%;
-    cursor: pointer;
-    animation: bounce 3s ease-in-out infinite alternate;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 12px 15px;
+  background: rgba(0, 0, 0, 0.7);
+  color: #fecf0a;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  animation: bounce 3s ease-in-out infinite alternate;
 
-    /* transform: scale(1); */
-    @keyframes bounce {
+  /* transform: scale(1); */
+  @keyframes bounce {
     0% {
-        transform: translate(-50%, -50%) scale(1);
+      transform: translate(-50%, -50%) scale(1);
     }
 
     50% {
-        transform: translate(-50%, -50%) scale(1.2);
+      transform: translate(-50%, -50%) scale(1.2);
     }
 
     100% {
-        transform: translate(-50%, -50%) scale(1);
+      transform: translate(-50%, -50%) scale(1);
     }
-    }
-`
+  }
+`;
 
-const VideoCard: React.FC<VideoCardProps> = ({ videoSrc, thumbnailSrc, altText = "Video preview" }) => {
-    const [isPlaying, setIsPlaying] = useState(false);
+const VideoCard: React.FC<VideoCardProps> = ({
+  videoSrc,
+  thumbnailSrc,
+  altText = 'Video preview',
+}) => {
+  const [isPlaying, setIsPlaying] = useState(false);
 
-    const handlePlay = () => {
-        setIsPlaying(true); // Меняем состояние, чтобы заменить превью на видео
-    };
+  const handlePlay = () => {
+    setIsPlaying(true); // Меняем состояние, чтобы заменить превью на видео
+  };
 
-    return (
-        <Card style={{
-            padding: "0",
-            position: "relative",
-            maxWidth: "640px",
-            overflow: "hidden",
-            width: "100%",
-        }}>
-            {!isPlaying ? (
-                <div style={{ position: "relative", cursor: "pointer" }}
-                    onClick={handlePlay}>
-                    {/* Превью видео */}
-                    <img
-                        src={thumbnailSrc}
-                        alt={altText}
-                        style={{
-                            width: "100%",
-                            display: "block",
-                        }}
-                    />
-                    {/* Кнопка Play */}
-                    <BoucingButton
-                        aria-label="Play Video"
-                    >
-                        ▶
-                    </BoucingButton>
-                </div>
-            ) : (
-                // Видео создается только после клика
-                <video
-                    playsInline
-                    controls
-                    autoPlay
-                    loop={false}
-                    muted={true}
-                    src={videoSrc}
-                    onEnded={() => {
-                        console.log("Video ended");
-                        setIsPlaying(false);
-                    }}
-                    style={{
-                        width: "100%",
-                        display: "block",
-                    }}
-                />
-
-            )}
-        </Card>
-    );
+  return (
+    <Card
+      style={{
+        padding: '0',
+        position: 'relative',
+        maxHeight: isPlaying ? 'unset' : '380px',
+        maxWidth: isPlaying ? '640px' : '100%',
+        overflow: 'hidden',
+        width: '100%',
+        margin: 'auto'
+      }}
+    >
+      {!isPlaying ? (
+        <div style={{ position: 'relative', cursor: 'pointer' }} onClick={handlePlay}>
+          {/* Превью видео */}
+          <img
+            src={thumbnailSrc}
+            alt={altText}
+            style={{
+              maxHeight: '380px',
+              width: '100%',
+              display: 'block',
+            }}
+          />
+          {/* Кнопка Play */}
+          <BoucingButton aria-label='Play Video'>▶</BoucingButton>
+        </div>
+      ) : (
+        // Видео создается только после клика
+        <video
+          playsInline
+          controls
+          autoPlay
+          loop={false}
+          muted={true}
+          src={videoSrc}
+          onEnded={() => {
+            console.log('Video ended');
+            setIsPlaying(false);
+          }}
+          style={{
+            width: '100%',
+            display: 'block',
+          }}
+        />
+      )}
+    </Card>
+  );
 };
 
 export default VideoCard;

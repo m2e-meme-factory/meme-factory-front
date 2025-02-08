d5c118bab65100d15f68de655736355fbd346c41
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2025-02-08 10:12:29 +0300

    fix: useAutoTasks

--- src/pages/ProfilePage/useAutoTasks.tsx
+++ src/pages/ProfilePage/useAutoTasks.tsx
@@ -8,58 +8,16 @@ import { CATEGORY_TASKS } from '@shared/consts/category-tasks';
 
 const useAutoTasks = () => {
   const [tonConnectUI] = useTonConnectUI();
-      const [tasks, setTasks] = useState<Task[]>([
-        { category: 'wallet', completed: tonConnectUI.connected },
-        ...baseTasks
-      ]);
-    
-      // Загрузка состояния задач из localStorage при монтировании компонента
-      useEffect(() => {
-        const savedTasks = localStorage.getItem('tasks');
-        if (savedTasks && savedTasks.length > 0) {
-          const parsedSavedTasks: Task[] = JSON.parse(savedTasks);
-          const savedCategories = new Set(parsedSavedTasks.map(t => t.category));
-          
-          // Добавляем задачи из baseTasks, которых нет в сохраненных
-          const missingBaseTasks = baseTasks.filter(t => !savedCategories.has(t.category));
-          let mergedTasks: Task[] = [...parsedSavedTasks, ...missingBaseTasks];
-          
-          // Проверяем наличие задачи 'wallet'
-          const hasWallet = parsedSavedTasks.some(t => t.category === 'wallet');
-          if (!hasWallet) {
-            mergedTasks = [{ category: 'wallet', completed: tonConnectUI.connected }, ...mergedTasks];
-          }
-          
-          setTasks(mergedTasks);
-        } else {
-          // Инициализируем с базовыми задачами и 'wallet'
-          setTasks([
-            { category: 'wallet', completed: tonConnectUI.connected },
-            ...baseTasks
-          ]);
-        }
-      }, []);
-    
-      useEffect(() => {
-        const unsubscribe = tonConnectUI.onStatusChange((wallet) => {
-          if (wallet) {
-            markTaskAsCompleted('wallet');
-          }
-        });
-    
-        return () => unsubscribe();
-      }, [tonConnectUI]);
-    
-      // Функция для отметки задачи выполненной
-      const markTaskAsCompleted = (category: string) => {
-        const newtasks = tasks.map((task) =>
-          task.category === category ? { ...task, completed: true } : task
-        );
-    
-        setTasks(newtasks);
-    
-        localStorage.setItem('tasks', JSON.stringify(newtasks));
-      };
+  const { data: mapAutoTasks, isLoading, isFetched } = useGetMapAutoTasks();
+  const { mutate: onClimeAutoTask } = useClimeAutoTask();
+
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  const handleMarkTaskAsCompleted = useCallback(
+    (category: string) => {
+      if (abortControllerRef.current) {
+        abortControllerRef.current.abort();
+      }
 
       const abortController = new AbortController();
       abortControllerRef.current = abortController;
